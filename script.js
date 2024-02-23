e0b88fe14cf4de4ab001a6ad94e988e4feaaa81a
Ester <espi0001@stud.kea.dk>
2024-02-23 15:45:18 +0100

    lidt kode

---
+++ script.js
@@ -0,0 +1,25 @@
+function highlightCurrentPage() {
+  var breadcrumbItems = document.querySelectorAll(".breadcrumb");
+
+  breadcrumbItems.forEach(function (item) {
+    // Check if the href attribute matches the current page URL
+    if (item.getAttribute("href") === window.location.pathname) {
+      item.classList.add(".active");
+    } else {
+      item.classList.remove(".active");
+    }
+  });
+}
+
+// Initial highlighting check in case the page is loaded with a specific URL
+highlightCurrentPage();
+
+const goTop = document.querySelector("#goTop");
+
+goTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
